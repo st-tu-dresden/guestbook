7434564eb1e92f13800577af77769df47f9df1c4
Paul Henke <paul.henke@outlook.com>
2014-08-11 19:25:36 +0200

    #3, #4, #6 - Further cleanups in JavaSCript and templates.
    Included 'use strict' in guestbook.js. Removed unused index.html. JavaScript cleanups: new formatting, tabs everywhere, added missing semicolons. Removed unused success parameter in success handler.
    
    Tidied up messages.properties. Modified guestbook.html to use new messages.properties.

--- src/main/resources/static/resources/js/guestbook.js
+++ src/main/resources/static/resources/js/guestbook.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	'use strict';
 	
 	$('#form').submit(function(e) {
 
@@ -7,26 +8,23 @@ $(document).ready(function() {
 		}
 		e.preventDefault();
 		
-		var form = $(this)
+		var form = $(this);
 		
 		$.ajax({
-			type     : 'POST',
-			cache    : false,
-			url      : form.attr('action'),
-			data     : form.serialize(),
-			success  : function(data) {
+			type	: 'POST',
+			cache	: false,
+			url		: form.attr('action'),
+			data	: form.serialize(),
+			success	: function(data) {
 				$("#entries").append(data);
-
-				$('html, body').animate({
-			        scrollTop: form.offset().top
-			    }, 2000);
-				
+				$('html, body').animate({scrollTop: form.offset().top}, 2000);
 				e.target.reset();
-			}});
+			}
 		});
+	});
 	
 	$('#entries').on('submit','form', function(e){
-	
+
 		if(!$('#use_ajax').is(':checked')) {
 			return;
 		}
@@ -36,18 +34,15 @@ $(document).ready(function() {
 		var id = form.attr('data-entry-id');
 		
 		$.ajax({
-			type     : 'DELETE',
-			cache    : false,
-			url      : form.attr('action'),
-			data     : form.serialize(),
-			success: function(success) {
+			type	: 'DELETE',
+			cache	: false,
+			url		: form.attr('action'),
+			data	: form.serialize(),
+			success	: function() {
 				$('#entry' + id).slideUp(500, function() {
 					$(this).remove();
 				});
 			}
 		});
-		
-		
-		}
-	)
-})
+	});
+});
