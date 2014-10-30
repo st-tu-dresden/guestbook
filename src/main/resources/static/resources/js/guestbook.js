43a16e520d54862f5ba60b828d7a11b4a7b625a2
Sascha Weichel <weichel.sascha@xxx.xx>
2014-10-30 14:05:11 +0100

    #13 - Fixed UI logic to fix glitches after adds and removals of entries.
    Fixed guestbook entry indexes in headlines after adding and removing entries. We now remove and add containing divs, too.

--- src/main/resources/static/resources/js/guestbook.js
+++ src/main/resources/static/resources/js/guestbook.js
@@ -1,47 +1,61 @@
 $(document).ready(function() {
 	'use strict';
-	
+
 	$('#form').submit(function(e) {
 
 		if(!$('#use_ajax').is(':checked')) {
 			return;
 		}
-		
+
 		e.preventDefault();
-		
+
 		var form = $(this);
-		
+
 		$.ajax({
 			type	: 'POST',
 			cache	: false,
 			url		: form.attr('action'),
 			data	: form.serialize(),
 			success	: function(data) {
-				$("#entries").append(data);
+				$("#entries").append('<div>' + data + '</div>');
+
+				// fix index
+				var index = $('#entries div[id^="entry"]').length;
+				var textArray = $(data).find('h3').text().split('.', 2);
+
+				$('#entries div[id^="entry"]:last').find('h3').text(index + '.' + textArray[1]);
 				$('html, body').animate({scrollTop: form.offset().top}, 2000);
+
 				e.target.reset();
 			}
 		});
 	});
-	
+
 	$('#entries').on('submit','form', function(e){
 
 		if(!$('#use_ajax').is(':checked')) {
 			return;
 		}
+
 		e.preventDefault();
+
 		var form = $(this);
-		
 		var id = form.attr('data-entry-id');
-		
+
 		$.ajax({
 			type	: 'DELETE',
 			cache	: false,
 			url		: form.attr('action'),
 			data	: form.serialize(),
 			success	: function() {
+
 				$('#entry' + id).slideUp(500, function() {
-					$(this).remove();
+					var followingEntries = $(this).parent().nextAll().each(function() {
+						var textArray = $(this).find('h3').text().split('.', 2);
+						$(this).find('h3').text((parseInt(textArray[0],10)-1) + '.' + textArray[1]);
+					});
+
+					$(this).parent().remove();
 				});
 			}
 		});
