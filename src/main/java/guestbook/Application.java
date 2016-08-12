20d8fadc80c5363d1f94b8927b3d4e76708eb243
Oliver Gierke <info@olivergierke.de>
2016-08-12 15:00:07 +0200

    #31 - Upgraded to Spring Boot 1.4 GA.
    Removed explicit configuration of JSR-310 converters as the Hibernate version Spring Boot 1.4 includes by default is able to persist JSR-310 types out of the box.
    
    Removed explicit disabling of the thymeleaf caching as the DevTools module enables that out of the box.
    
    Added Webjars locator library so that the template can use references to JavaScript libraries without referring to versions.

--- src/main/java/guestbook/Application.java
+++ src/main/java/guestbook/Application.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2015 the original author or authors.
+ * Copyright 2014-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,10 +21,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 /**
@@ -37,7 +35,6 @@ import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
  * @author Oliver Gierke
  */
 @SpringBootApplication
-@EntityScan(basePackageClasses = { Application.class, Jsr310JpaConverters.class })
 public class Application {
 
 	/**
