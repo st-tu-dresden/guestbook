624dcd3f31a2673d7d4145a119efea783f0c05ad
3m5.Lukas Dams <lukas.dams@3m5.de>
2020-10-30 11:05:57 +0100

    Add EntryColor class

---
+++ src/main/java/guestbook/EntryColor.java
@@ -0,0 +1,7 @@
+package guestbook;
+
+public class EntryColor {
+    public static final String GREEN = "green";
+    public static final String RED = "red";
+    public static final String BLUE = "blue";
+}
