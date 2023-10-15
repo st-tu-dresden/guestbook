7510ce9f9216f0a2bc4b65432f72ebf150144542
theBashtler <thebashtler@broken.com>
2023-10-15 17:51:24 +0200

    Add Docstring for numberOfEntries function

--- src/main/java/guestbook/GuestbookService.java
+++ src/main/java/guestbook/GuestbookService.java
@@ -8,7 +8,13 @@ import java.time.LocalDateTime;
 @Service
 public class GuestbookService {
 
-    public static long numberOfEntries(GuestbookRepository repository){
+    /**
+     * Calculates the number of entries in the current repository.
+     *
+     * @param repository the repo used to save the guestbook entries
+     * @return {@code long} containing the number of entries
+     */
+    public static long numberOfEntries(GuestbookRepository repository) {
         return repository.count();
     }
 }
